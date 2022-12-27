2c1e455955b6b2ea355411b065520bb335036e97
4multimedia <biuro@4multimedia.pl>
2022-12-28 00:21:53 +0100

    1.0.19

--- src/Core/resources/views/layout/app.blade.php
+++ src/Core/resources/views/layout/app.blade.php
@@ -1,9 +1,16 @@
-{{ get_css() }}
-<div id="app">
-	<div>{!! do_menu() !!}</div>
-	<div>
-    	@yield('content')
-	</div>
-</div>
-
-<script src="{{ mix('js/backend.js') }}"></script>
+<html lang="en" class="light">
+    <head>
+        <link rel="stylesheet" href="{{ mix('css/backend.css') }}" />
+        {{ get_css() }}
+        <title>Admin</title>
+    </head>
+    <body class="py-5 md:py-0 bg-black/[0.15] dark:bg-transparent">
+        <div id="app">
+            <div>{!! do_menu() !!}</div>
+            <div>
+                @yield('content')
+            </div>
+        </div>
+        <script src="{{ mix('js/backend.js') }}"></script>
+    </body>
+</html>
--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -1,6 +1,8 @@
 let mix = require('laravel-mix');
+require('mix-tailwindcss');
 
 mix.vue();
-mix.js('Core/resources/js/app.js', 'js/backend.js').setPublicPath('../../../../public');
+mix.js('Core/resources/js/app.js', 'js/backend.js').setPublicPath('../../../public');
+mix.postCss('Core/resources/css/app.css', 'css/backend.css').setPublicPath('../../../public').tailwind();
 
 // DODATKOWE CSS
