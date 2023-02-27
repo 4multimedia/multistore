f263293f171275712afa23f836a5c4b21a61b421
4multimedia <9784148+4multimedia@users.noreply.github.com>
2023-02-27 15:08:17 +0100

    1.0.149

--- src/Core/Http/Components/Input/Field.php
+++ src/Core/Http/Components/Input/Field.php
@@ -12,17 +12,23 @@
 		public $url;
         public $items = [];
         public $selected = [];
+		public $params = [];
+		public $watch;
 
-        public function __construct($id = null, $label = null, $items = [], $selected = [], $name = null, $url = null) {
+        public function __construct($id = null, $id_directory = null, $label = null, $items = [], $selected = [], $name = null, $url = null, $params = [], $watch = []) {
             $this->id = $id;
             $this->label = $label;
-			$this->items = $items;
-			if ($id) {
+			$this->id = $id;
+			if ($id_directory) {
             	$this->items = get_dictionary($id);
 			}
 			$this->name = $name;
             $this->selected = $selected;
 			$this->url = $url;
+			$this->params = json_encode($params);
+			foreach($watch as $key => $value) {
+				$this->watch .= '$watch(\''.$key.'\', value => '.$value.'); ';
+			}
         }
 
 		public function render() {
--- src/Core/resources/views/components/input/dropdown.blade.php
+++ src/Core/resources/views/components/input/dropdown.blade.php
@@ -1,15 +1,32 @@
-<div x-data="{open: false, value: {}, options:[]}" @if($url) x-init="options = await (await axios.post('{{ $url }}'))" @endif @click.outside="open = false" class="form-dropdown" :class="open ? 'form-dropdown-open' : ''">
+<div x-data="data()" x-init="{{ $watch }} $watch('value', newValue => console.log(newValue)); loadOptions('{{ $url ? $url : null }}', {{ $params }})" @click.outside="open = false" class="form-dropdown" :class="open ? 'form-dropdown-open' : ''">
 	@if ($label) <label class="form-label">{{ $label }}</label> @endif
-	<input type="hidden" name="{{ $name }}" x-model="value.id" />
+	<input type="text" x-ref="{{ $id }}" name="{{ $name }}" x-model="value.id" />
 	<div class="form-dropdown-value" x-on:click="open = !open">
 		<span x-text="value.name"></span>
 		<div class="handler"><span class="material-icons-outlined">expand_more</span></div>
 	</div>
-	<div x-show="open" class="form-dropdown-list">
+	<div x-show="open" x-transition class="form-dropdown-list">
 		<ul>
 			<template x-for="option in options">
-				<li x-on:click="value = option"><span x-text="option.name"></span></li>
+				<li x-on:click="value = option; open = false;"><span x-text="option.name"></span></li>
 			</template>
 		</ul>
 	</div>
 </div>
+
+<script>
+	function data() {
+		return {
+			open: false,
+			value: {},
+			options: [],
+			async loadOptions(url, params) {
+				this.options = [];
+				if (url) {
+					const request = await window.axios.post(url, params);
+					this.options = request.data
+				}
+			}
+		}
+	}
+</script>
