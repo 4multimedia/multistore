e320c14c66207a12c7037d6b7ebacf7060cf25a0
4multimedia <9784148+4multimedia@users.noreply.github.com>
2023-03-03 07:30:48 +0100

    1.0.176

--- src/Core/Http/Controllers/Api/Content/CategoryController.php
+++ src/Core/Http/Controllers/Api/Content/CategoryController.php
@@ -2,22 +2,22 @@
 
     namespace Multimedia\Multistore\Core\Http\Controllers\Api\Content;
 
-	use App\Modules\Product\Http\Models\Category;
 	use Illuminate\Http\Request;
 	use Multimedia\Multistore\Core\Http\Controllers\Controller;
 	use Illuminate\Support\Str;
+	use Multimedia\Product\Models\ProductCategory;
 
     class CategoryController extends Controller
     {
         public function index(Request $request) {
-            return Category::where('id_product_category_parent', $request->id_parent == 'null' ? NULL : $request->id_parent)->get()->toTree();
+            return ProductCategory::where('id_product_category_parent', $request->id_parent == 'null' ? NULL : $request->id_parent)->get()->toTree();
         }
 
 		public function store(Request $request) {
-			return Category::create([
+			return ProductCategory::create([
 				'id_product_category_parent' => $request->id_parent,
 				'name' => ['pl' => $request->name],
-				'slug' => ['pl' => Str::slug($request->name)]
+				'slug' => $request->name
 			]);
 		}
     }
