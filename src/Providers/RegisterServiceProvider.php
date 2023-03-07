209a3d3da8316f27c6160efa54dadea5d28ae851
4multimedia <9784148+4multimedia@users.noreply.github.com>
2023-03-07 15:00:58 +0100

    1.0.194

--- src/Providers/RegisterServiceProvider.php
+++ src/Providers/RegisterServiceProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\ServiceProvider;
 use Multimedia\Multistore\Classes\Hooks;
 use Multimedia\Multistore\Classes\Backend;
 use Multimedia\Multistore\Classes\Media;
+use Multimedia\Multistore\Classes\Modules;
 use Multimedia\Multistore\Classes\Plugins;
 use Multimedia\Multistore\Classes\Form;
 use Multimedia\Multistore\Classes\Menu;
@@ -19,55 +20,55 @@ use Multimedia\Multistore\Classes\Slug;
 class RegisterServiceProvider extends ServiceProvider {
 
 	public function register() {
-		$this->app->bind('hooks', function($app) {
+		$this->app->singleton('hooks', function($app) {
 			return new Hooks();
 		});
 
-		$this->app->bind('modules', function ($app) {
-			return $app->make(\Multimedia\Multistore\Classes\Modules::class);
+		$this->app->singleton('modules', function($app) {
+			return new Modules();
 		});
 
-		$this->app->bind('plugins', function($app) {
+		$this->app->singleton('plugins', function($app) {
 			return new Plugins();
 		});
 
-        $this->app->bind('form', function($app) {
+        $this->app->singleton('form', function($app) {
 			return new Form();
 		});
 
-		$this->app->bind('menu', function($app) {
+		$this->app->singleton('menu', function($app) {
 			return new Menu();
 		});
 
-        $this->app->bind('user_log', function($app) {
+        $this->app->singleton('user_log', function($app) {
 			return new UserLog();
 		});
 
-		$this->app->bind('backend', function($app) {
+		$this->app->singleton('backend', function($app) {
 			return new Backend();
 		});
 
-		$this->app->bind('media', function($app) {
+		$this->app->singleton('media', function($app) {
 			return new Media();
 		});
 
-		$this->app->bind('table', function($app) {
+		$this->app->singleton('table', function($app) {
 			return new Tables();
 		});
 
-		$this->app->bind('domain', function($app) {
+		$this->app->singleton('domain', function($app) {
 			return new Domain();
 		});
 
-		$this->app->bind('layout', function($app) {
+		$this->app->singleton('layout', function($app) {
 			return new Layout();
 		});
 
-        $this->app->bind('page', function($app) {
+        $this->app->singleton('page', function($app) {
 			return new Page();
 		});
 
-        $this->app->bind('slug', function($app) {
+        $this->app->singleton('slug', function($app) {
 			return new Slug();
 		});
 	}
