1392ae19a793d0b039c57b18773b9c4de1d53345
4multimedia <9784148+4multimedia@users.noreply.github.com>
2023-03-07 13:16:20 +0100

    1.0.185

--- src/Providers/AssetsServiceProvider.php
+++ src/Providers/AssetsServiceProvider.php
@@ -9,12 +9,14 @@ use Str;
 class AssetsServiceProvider extends ServiceProvider {
 
 	public function register() {
-		$this->app->singleton('hooks', function($app) {
-			return new Hooks();
-		});
+
 	}
 
 	public function boot() {
+		$this->app->singleton('hooks', function($app) {
+			return new Hooks();
+		});
+
 		generate_css_variables();
 		generate_css_from_layout();
 	}
