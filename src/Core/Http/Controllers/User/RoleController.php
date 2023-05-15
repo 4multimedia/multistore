8f898cf4990cd6d4ae0e56b0a9a80a90212e699f
4multimedia <9784148+4multimedia@users.noreply.github.com>
2023-05-15 08:20:41 +0200

    1.0.232

--- src/Core/Http/Controllers/User/RoleController.php
+++ src/Core/Http/Controllers/User/RoleController.php
@@ -3,11 +3,28 @@
     namespace Multimedia\Multistore\Core\Http\Controllers\User;
 
     use App\Http\Controllers\Controller;
+	use Multimedia\Multistore\Core\Http\Classes\Table;
+	use Multimedia\Multistore\Core\Models\UserRole;
 
     class RoleController extends Controller
     {
         public function index() {
-			return view('backend::user.role.index');
+			set_title(__('backend::page.title'));
+            add_action_header(__('backend::page.create'), route('backend.page.create'));
+
+			$table = new Table('user_role', [
+				'query' => UserRole::paginate(15)
+			]);
+
+            $table->set_header('ID', ['id' => 'id', 'headerClass' => 'w-16']);
+            $table->set_header('Nazwa', ['id' => 'name', 'template' => '<a href="'.route('backend.page.update', ['hash' => '$hash']).'" class="underline decoration-dotted whitespace-nowrap">$name</a>']);
+            $table->set_action('edit', 'Edycja', route('backend.user.role.update', ['hash' => '$hash']));
+            $table->set_action('delete', 'Usuń', route('backend.user.role.delete', ['hash' => '$hash']), ['method' => 'delete']);
+
+            $data = [];
+			$data['table'] = $table->render();
+
+			return view('backend::user.role.index', $data);
         }
 
 		public function create() {
--- src/Core/resources/views/user/role/index.blade.php
+++ src/Core/resources/views/user/role/index.blade.php
@@ -1,5 +1,5 @@
 @extends('backend::layouts.app')
 
 @section('content')
-
+{!! $table !!}
 @endsection
